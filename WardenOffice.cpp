9c5ee905a2781e6c351213f3e274c91081724348
laychris93 <43182761+laychris93@users.noreply.github.com>
2019-04-04 12:38:18 -0500

    Update WardenOffice.cpp

--- WardenOffice.cpp
+++ WardenOffice.cpp
@@ -1,6 +1,6 @@
 /********************************************************************* 
 * ** Program Name: WardenOffice Class
-* ** Author: Chris Lay
+* ** Author: Jeremy Lay
 * ** Date: 12/24/2018  
 * ** Description: Implemenation file for WardenOffice class, derived from 
 * ** Space class.  
