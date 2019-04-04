02383e685e2ac689ba4926675df978e9d6f0ef4a
laychris93 <43182761+laychris93@users.noreply.github.com>
2019-04-04 12:43:07 -0500

    Update Hospital.hpp

--- Hospital.hpp
+++ Hospital.hpp
@@ -1,6 +1,6 @@
 /********************************************************************* 
 * ** Program Name: Hospital Class
-* ** Author: Chris Lay
+* ** Author: Jeremy Lay
 * ** Date: 12/24/2018  
 * ** Description: Header file for Hospital class, derived from Space
 * ** class.  Class specific variables include bool for whether player
