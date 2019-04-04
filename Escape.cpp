120ff17a06de5f3284b5a84dec32feabea42f865
laychris93 <43182761+laychris93@users.noreply.github.com>
2019-04-04 12:36:49 -0500

    Update Escape.cpp

--- Escape.cpp
+++ Escape.cpp
@@ -1,6 +1,6 @@
 /********************************************************************* 
 * ** Program Name: Escape Class
-* ** Author: Chris Lay
+* ** Author: Jeremy Lay
 * ** Date: 12/24/2018  
 * ** Description: Implementation file for Escape class, derived from Space
 * ** class.  
